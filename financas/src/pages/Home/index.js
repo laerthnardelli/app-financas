5ebca37be1c10e8c3d43d9b92aaecc9c3282dcde
laerthnardelli <laerthnardelli@gmail.com>
2020-08-28 19:55:05 -0300

    Excluindo movimentação
     Excluindo movimentação

--- financas/src/pages/Home/index.js
+++ financas/src/pages/Home/index.js
@@ -1,5 +1,5 @@
 import React, {useContext, useState, useEffect} from 'react';
-import {View, Text, Button} from 'react-native';
+import {Alert} from 'react-native';
 import firebase from '../../services/firebaseConnection';
 import {format} from 'date-fns';
 
@@ -41,6 +41,7 @@ export default function Home() {
               key: childItem.key,
               tipo: childItem.val().tipo,
               valor: childItem.val().valor,
+              date: childItem.val().date,
             };
 
             setHistorico((oldArray) => [...oldArray, list].reverse());
@@ -50,6 +51,48 @@ export default function Home() {
 
     loadList();
   }, []);
+
+  function handleDelete(data) {
+    Alert.alert(
+      'Cuidado Atençao!',
+      `Você deseja excluir ${data.tipo} - Valor: ${data.valor}`,
+      [
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+        {
+          text: 'Continuar',
+          onPress: () => handleDeleteSuccess(data),
+        },
+      ],
+    );
+  }
+
+  async function handleDeleteSuccess(data) {
+    await firebase
+      .database()
+      .ref('historico')
+      .child(uid)
+      .child(data.key)
+      .remove()
+      .then(async () => {
+        let saldoAtual = saldo;
+        data.tipo === 'despesa'
+          ? (saldoAtual += parseFloat(data.valor))
+          : (saldoAtual -= parseFloat(data.valor));
+
+        await firebase
+          .database()
+          .ref('users')
+          .child(uid)
+          .child('saldo')
+          .set(saldoAtual);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <Background>
@@ -67,7 +110,9 @@ export default function Home() {
         showsVerticalScrollIndicator={false}
         data={historico}
         keyExtractor={(item) => item.key}
-        renderItem={({item}) => <HistoricoList data={item} />}
+        renderItem={({item}) => (
+          <HistoricoList data={item} deleteItem={handleDelete} />
+        )}
       />
     </Background>
   );
