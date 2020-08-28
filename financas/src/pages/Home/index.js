2707da6710e995c6429143dde588f3eb50eff5aa
laerthnardelli <laerthnardelli@gmail.com>
2020-08-28 14:57:50 -0300

    Criando Tela Home pt2
    Criando Tela Home pt2

--- financas/src/pages/Home/index.js
+++ financas/src/pages/Home/index.js
@@ -1,12 +1,24 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import {View, Text, Button} from 'react-native';
 
 import {AuthContext} from '../../contexts/auth';
 import Header from '../../components/Header';
+import HistoricoList from '../../components/HistoricoList';
 
-import {Background, Container, Nome, Saldo, Title} from './styles';
+import {Background, Container, Nome, Saldo, Title, List} from './styles';
 
 export default function Home() {
+  const [historico, setHistorico] = useState([
+    {key: '1', tipo: 'receita', valor: 1200},
+    {key: '2', tipo: 'despesa', valor: 200},
+    {key: '3', tipo: 'receita', valor: 40},
+    {key: '4', tipo: 'receita', valor: 89.62},
+    {key: '5', tipo: 'despesa', valor: 500},
+    {key: '6', tipo: 'despesa', valor: 310},
+    {key: '7', tipo: 'despesa', valor: 380},
+    {key: '8', tipo: 'despesa', valor: 390},
+  ]);
+
   const {user} = useContext(AuthContext);
 
   return (
@@ -18,6 +30,13 @@ export default function Home() {
       </Container>
 
       <Title>Ultimas movimentações</Title>
+
+      <List
+        showsVerticalScrollIndicator={false}
+        data={historico}
+        keyExtractor={(item) => item.key}
+        renderItem={({item}) => <HistoricoList data={item} />}
+      />
     </Background>
   );
 }
